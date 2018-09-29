13e5c70fc55aa506d8c03d4669883e449054b364
Veniamin Tsigourof <tsigourof_ben@hotmail.com>
2018-09-29 23:43:43 +0300

    feat: created basic navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Componets/header';
+// import Header from './Componets/header';
 import Main from './Componets/main';
 import Footer from './Componets/footer';
+import Nav from './Componets/nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        {/* <Header/> */}
+        <Nav/>
         <Main/>
         <Footer/>
       </div>
--- src/Componets/nav.js
+++ src/Componets/nav.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './CSS/nav.css';
 
+// || My Story || My Resume || My Past Experience || My Projects || Blog || <-> || Contact Me ||
+
 function Nav() {
     return (
-        <nav>this will be the navigation bar</nav>
+        <nav>
+            <a href="#" className="nav-link my-story">My Story</a>
+            <a href="#" className="nav-link">My Resume</a>
+            <a href="#" className="nav-link">My Past Experience</a>
+            <a href="#" className="nav-link">My Projects</a>
+            <a href="#" className="nav-link">Blog</a>
+            <a href="#" className="nav-link">Contact Me</a>
+        </nav>
     )
 }
 
