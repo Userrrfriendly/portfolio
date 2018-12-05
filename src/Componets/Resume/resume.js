b09f7633b030e8af5d71a9f305acac9ed7721852
Veniamin Tsigourof <tsigourof_ben@hotmail.com>
2018-12-05 16:07:50 +0200

    chore: upgrated reactscripts to version: 2.1.1

--- src/Componets/Resume/resume.js
+++ src/Componets/Resume/resume.js
@@ -3,10 +3,10 @@ import './resume.css';
 
 function Resume() {
     return(
-        <div className="error-route">
-          <h1>Download my resume as PDF:</h1>
-          <a href="../../assets/Alpha Version03.pdf" download="Ben's Resume.pdf">Download</a>  
-        </div>
+      <div className="error-route">
+        <h1>Download my resume as PDF:</h1>
+        <a href="../../assets/Alpha Version03.pdf" download="Ben's Resume.pdf">Download</a>  
+      </div>
     )
 }
 export default Resume;
