0861a0e300ebc2ca3f2813605f0290ffe330b301
Veniamin Tsigourof <tsigourof_ben@hotmail.com>
2019-02-27 17:03:21 +0200

    Updates

---
+++ precache-manifest.c4f460dd4e7a11fff766d3e035a5baba.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e968f6f0f47e9c8172e",
+    "url": "/portfolio/static/css/main.3758419f.chunk.css"
+  },
+  {
+    "revision": "0e968f6f0f47e9c8172e",
+    "url": "/portfolio/static/js/main.0e968f6f.chunk.js"
+  },
+  {
+    "revision": "e475a9a9c1eefbf77761",
+    "url": "/portfolio/static/js/1.e475a9a9.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4afa0ba2924be3cedb118bdbe625b877",
+    "url": "/portfolio/static/media/flickr-photo-plotter.4afa0ba2.jpg"
+  },
+  {
+    "revision": "ca8901981b5ded6a3bb71805e0236d35",
+    "url": "/portfolio/static/media/arcade-1.ca890198.jpg"
+  },
+  {
+    "revision": "fda4273cfd4ed290cc434fcd5266c985",
+    "url": "/portfolio/static/media/matching-game.fda4273c.jpg"
+  },
+  {
+    "revision": "9c9ea40b2857ea8027e1627ff86a52e8",
+    "url": "/portfolio/static/media/myReads.9c9ea40b.jpg"
+  },
+  {
+    "revision": "ce338827b22afb5ae8dfdae0f61c4c83",
+    "url": "/portfolio/static/media/jasmine.ce338827.jpg"
+  },
+  {
+    "revision": "7fb344495a6af8e75b980b9e6b11b4b8",
+    "url": "/portfolio/static/media/restaurant-review.7fb34449.jpg"
+  },
+  {
+    "revision": "234ff7b5b009e9220eb74f7ee9c7da4f",
+    "url": "/portfolio/static/media/Wikipedia Viewer.234ff7b5.jpg"
+  },
+  {
+    "revision": "441752af0ec423181efb2bf20455cc19",
+    "url": "/portfolio/static/media/weather-app.441752af.jpg"
+  },
+  {
+    "revision": "108d796b5ab94a1049bda1c885e451f8",
+    "url": "/portfolio/static/media/calculator.108d796b.jpg"
+  },
+  {
+    "revision": "95a56a88e7867909821a769116ee2eed",
+    "url": "/portfolio/static/media/hero-medium.95a56a88.jpg"
+  },
+  {
+    "revision": "c55e4264cafd8c49445b0363148761b6",
+    "url": "/portfolio/static/media/hero-small.c55e4264.jpg"
+  },
+  {
+    "revision": "596b156bd00ddcbcb97bebda220a1538",
+    "url": "/portfolio/static/media/this-is-me.596b156b.jpg"
+  },
+  {
+    "revision": "b1f6e48d757b2d01fcba73774150e4c0",
+    "url": "/portfolio/static/media/hero-large_h1.b1f6e48d.jpg"
+  },
+  {
+    "revision": "70167370e72da673ad8eada590b52735",
+    "url": "/portfolio/static/media/resume_Jupiter.70167370.jpg"
+  },
+  {
+    "revision": "14b1d62e817787aac755d2a96da49f0a",
+    "url": "/portfolio/static/media/about-me.14b1d62e.jpg"
+  },
+  {
+    "revision": "99d5f929e369ee674887bc5b3cd67165",
+    "url": "/portfolio/static/media/download - w.99d5f929.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/portfolio/static/media/Hamburger_icon.86595150.svg"
+  },
+  {
+    "revision": "b763be2d4a2bd8fcd25e1a7297d74a9b",
+    "url": "/portfolio/index.html"
+  }
+];
