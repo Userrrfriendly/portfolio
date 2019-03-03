e8723f397e16425569a97fb908d9ea3c3e18c445
Veniamin Tsigourof <tsigourof_ben@hotmail.com>
2019-03-03 15:14:27 +0200

    Updates

---
+++ precache-manifest.dfeb5d6449e749c45b84bbcf233459ca.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "a51445a2f9071257e3cd",
+    "url": "/static/css/main.94f8da52.chunk.css"
+  },
+  {
+    "revision": "a51445a2f9071257e3cd",
+    "url": "/static/js/main.a51445a2.chunk.js"
+  },
+  {
+    "revision": "c42f4fc9c1c0c1592cc4",
+    "url": "/static/css/1.c264d458.chunk.css"
+  },
+  {
+    "revision": "c42f4fc9c1c0c1592cc4",
+    "url": "/static/js/1.c42f4fc9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4afa0ba2924be3cedb118bdbe625b877",
+    "url": "/static/media/flickr-photo-plotter.4afa0ba2.jpg"
+  },
+  {
+    "revision": "ca8901981b5ded6a3bb71805e0236d35",
+    "url": "/static/media/arcade-1.ca890198.jpg"
+  },
+  {
+    "revision": "fda4273cfd4ed290cc434fcd5266c985",
+    "url": "/static/media/matching-game.fda4273c.jpg"
+  },
+  {
+    "revision": "9c9ea40b2857ea8027e1627ff86a52e8",
+    "url": "/static/media/myReads.9c9ea40b.jpg"
+  },
+  {
+    "revision": "ce338827b22afb5ae8dfdae0f61c4c83",
+    "url": "/static/media/jasmine.ce338827.jpg"
+  },
+  {
+    "revision": "7fb344495a6af8e75b980b9e6b11b4b8",
+    "url": "/static/media/restaurant-review.7fb34449.jpg"
+  },
+  {
+    "revision": "234ff7b5b009e9220eb74f7ee9c7da4f",
+    "url": "/static/media/Wikipedia Viewer.234ff7b5.jpg"
+  },
+  {
+    "revision": "441752af0ec423181efb2bf20455cc19",
+    "url": "/static/media/weather-app.441752af.jpg"
+  },
+  {
+    "revision": "108d796b5ab94a1049bda1c885e451f8",
+    "url": "/static/media/calculator.108d796b.jpg"
+  },
+  {
+    "revision": "95a56a88e7867909821a769116ee2eed",
+    "url": "/static/media/hero-medium.95a56a88.jpg"
+  },
+  {
+    "revision": "c55e4264cafd8c49445b0363148761b6",
+    "url": "/static/media/hero-small.c55e4264.jpg"
+  },
+  {
+    "revision": "596b156bd00ddcbcb97bebda220a1538",
+    "url": "/static/media/this-is-me.596b156b.jpg"
+  },
+  {
+    "revision": "b1f6e48d757b2d01fcba73774150e4c0",
+    "url": "/static/media/hero-large_h1.b1f6e48d.jpg"
+  },
+  {
+    "revision": "70167370e72da673ad8eada590b52735",
+    "url": "/static/media/resume_Jupiter.70167370.jpg"
+  },
+  {
+    "revision": "14b1d62e817787aac755d2a96da49f0a",
+    "url": "/static/media/about-me.14b1d62e.jpg"
+  },
+  {
+    "revision": "99d5f929e369ee674887bc5b3cd67165",
+    "url": "/static/media/download - w.99d5f929.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/Hamburger_icon.86595150.svg"
+  },
+  {
+    "revision": "8d0850b4afc09401de01813761c7e013",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fe841b8e76fb72b4713f3842913597a0.js"
+  "/precache-manifest.dfeb5d6449e749c45b84bbcf233459ca.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
